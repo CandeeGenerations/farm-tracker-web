ce40e20d28edbddb2560c97fe7b40a2ee3ee03ad
Tyler C <tyler@candeegenerations.com>
2025-05-08 13:23:09 -0400

    fix: date fix

--- server/src/domains/sale/morphs.ts
+++ server/src/domains/sale/morphs.ts
@@ -13,7 +13,7 @@ export const morphExternalSale = (source: IExternalSale): Sale =>
       customerName: ({customerName}: IExternalSale) => customerName,
       notes: ({notes}: IExternalSale) => (notes ? notes.replace(/\\n/g, '\n') : null),
       amount: ({amount}: IExternalSale) => parseFloat(amount.toString()),
-      saleDate: ({saleDate}: IExternalSale) => (saleDate ? new Date(saleDate) : null),
+      saleDate: ({saleDate}: IExternalSale) => (saleDate ? new Date(`${saleDate}T00:00:00-04:00`) : null),
     },
     source as any,
   ) as Sale
