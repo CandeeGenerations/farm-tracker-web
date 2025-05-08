f2275f5e8f2091f0a613bea7b595768786b4b827
Tyler C <tyler@candeegenerations.com>
2025-05-08 13:31:09 -0400

    fix: another day fix

--- server/src/domains/sale/morphs.ts
+++ server/src/domains/sale/morphs.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {Sale} from '@generated/client'
+import dayjs from 'dayjs'
 import morphism from 'morphism'
 
 import {IExternalSale} from '../product-sale/types'
@@ -13,7 +14,8 @@ export const morphExternalSale = (source: IExternalSale): Sale =>
       customerName: ({customerName}: IExternalSale) => customerName,
       notes: ({notes}: IExternalSale) => (notes ? notes.replace(/\\n/g, '\n') : null),
       amount: ({amount}: IExternalSale) => parseFloat(amount.toString()),
-      saleDate: ({saleDate}: IExternalSale) => (saleDate ? new Date(`${saleDate}T00:00:00-04:00`) : null),
+      saleDate: ({saleDate}: IExternalSale) =>
+        saleDate ? new Date(`${dayjs(saleDate).format('YYYY-MM-DD')}T00:00:00-04:00`) : null,
     },
     source as any,
   ) as Sale
