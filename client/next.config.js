c2fc4439a7a35d2d559a49082f85f97beb327ceb
Tyler C <tyler@candeegenerations.com>
2025-05-08 13:28:07 -0400

    fix: trying to fix netlify

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  trailingSlash: false,
 }
 
 module.exports = nextConfig
