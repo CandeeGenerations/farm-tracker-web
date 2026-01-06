aa2ff9183731381e7744fb9bde8ade9ad75d4a2c
Tyler C <tyler@candeegenerations.com>
2026-01-06 15:44:16 -0500

    chore: build fix

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,4 +1,11 @@
-export {default} from 'next-auth/middleware'
+import {withAuth} from 'next-auth/middleware'
+
+export default withAuth({
+  callbacks: {
+    authorized: ({token}) => !!token,
+  },
+})
+
 export const config = {
   matcher: ['/animals/:path*', '/home/:path*', '/products/:path*', '/sales/:path*'],
 }
